4e200b563d9f95de11f7506cb90ddde69f83e256
Jackson Dickison <jaksondikison@gmail.com>
2019-09-03 19:41:18 -0700

    add parallax to navbar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -13,14 +13,14 @@ export default props => (
           height: 100vh;
           overflow-x: hidden;
           overflow-y: scroll;
-          perspective: 1px;
+          perspective: 2px;
           perspective-origin: 0 0;
         }
 
         .background {
-          height: 100vh;
+          height: 130vh;
           transform-origin: 0 0;
-          transform: translateZ(-1px) scale(2);
+          transform: translateZ(-2px) scale(2);
           background-image: url(../static/landing-background-cropped.png);
           background-size: cover;
           background-positon: top;
@@ -31,12 +31,24 @@ export default props => (
           position: absolute;
           top: 0;
           left: 0;
-          height: 100vh;
+          height: 130vh;
           width: 100vw;
           background-image: url(../static/layer1.png);
           background-size: cover;
           background-position: top;
           background-repeat: no-repeat;
+        }
+
+        .navbar {
+          position: absolute;
+          top: 72vh;
+          height: 17vh;
+          width: 100vw;
+          background-color: gray;
+          // border: 1px solid red;
+          z-index: 3;
+          transform-origin: 0 0;
+          transform: translateZ(1px) scale(0.5);
         }
       `}
     </style>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,20 +3,9 @@ import Layout from "../components/Layout";
 export default () => (
   <Layout>
     <div className="parallax-container">
-      <div className="background"></div>
-      <div className="foreground">
-        <div
-          className="nav-bar"
-          style={{
-            height: "9vh",
-            backgroundColor: "black",
-            color: "white",
-            marginTop: "100vh"
-          }}
-        >
-          about contact work
-        </div>
-      </div>
+      <div className="background" />
+      <div className="foreground" />
+      <div className="navbar" />
     </div>
   </Layout>
 );
