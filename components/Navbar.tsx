9619d972c1a6cbcba84f9d44ea477ef5e1eb52bc
Jackson Dickison <jaksondikison@gmail.com>
2019-09-07 09:40:35 -0700

    finish prototype of Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,17 +6,17 @@ interface NavbarProps {
 export default (props: NavbarProps) => {
   return (
     <div
-      className={`navbar absolute bg-gray-300 flex justify-center ${props.className}`}
+      className={`navbar absolute bg-gray-200 flex justify-center ${props.className}`}
       style={{ height: "17vh", width: "100vw", top: props.vhFromTop }}
     >
       <div className="flex" style={{ height: "100%", width: "80%" }}>
-        <div className="flex-grow flex justify-center items-center text-gray-700 text-2xl hover:text-black">
+        <div className="flex-grow flex justify-center items-center text-gray-800 text-2xl">
           WORK
         </div>
-        <div className="flex-grow flex justify-center items-center text-gray-700 text-2xl hover:text-black">
+        <div className="flex-grow flex justify-center items-center text-gray-800 text-2xl">
           ABOUT
         </div>
-        <div className="flex-grow flex justify-center items-center text-gray-700 text-2xl hover:text-black">
+        <div className="flex-grow flex justify-center items-center text-gray-800 text-2xl">
           CONTACT
         </div>
       </div>
