575c85b3571350e44b9cd35e6c959158621485d4
Jackson Dickison <jaksondikison@gmail.com>
2019-09-09 21:44:40 -0700

    make popup menu items hoverable and bottom item underlined by default

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,55 +6,99 @@ interface NavbarProps {
 export default (props: NavbarProps) => {
   return (
     <div
-      className={`navbar absolute bg-white flex justify-center border-solid border-t-2 border-gray-300 ${props.className}`}
-      style={{ height: "17vh", width: "100vw", top: props.vhFromTop }}
+      className={`navbar absolute bg-white flex justify-center ${props.className}`}
+      style={{
+        height: "17vh",
+        width: "100vw",
+        boxShadow:
+          "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)",
+        top: props.vhFromTop
+      }}
     >
       <div className="flex" style={{ height: "100%", width: "80%" }}>
         <div className="relative flex-grow flex justify-center items-center">
           <style jsx>{`
             .menu {
               position: absolute;
-              bottom: -17vh;
+              top: 17vh;
             }
 
-            .menu-older-sibling:hover + .menu {
-              position: absolute;
+            .menu-container:hover .menu {
+              //position: absolute;
               bottom: 17vh;
+              top: auto;
+            }
+
+            .non-option:hover .bottom-item {
+              text-decoration: underline;
+            }
+
+            .higher-item:hover ~ .bottom-item {
+              text-decoration: none;
             }
           `}</style>
           <div
             style={{
               width: "75%",
-              height: "100%",
-              top: "-2px"
+              height: "100%"
+              // top: "-2px"
             }}
-            className="menu-older-sibling flex justify-center items-center relative text-gray-800 text-2xl hover:text-black"
+            className="non-option menu-container flex justify-center items-center relative text-black text-2xl"
           >
-            WORK
+            <div>WORK</div>
+            <div className="menu text-xl text-black bg-white">
+              <div
+                className="higher-item pl-4 pr-4 hover:underline"
+                style={{ padding: "1.5rem 2rem .25rem 2rem" }}
+              >
+                ILLUSTRATION
+              </div>
+              <div
+                className="higher-item pl-4 pr-4 hover:underline"
+                style={{ padding: ".25rem 2rem" }}
+              >
+                ANIMATION
+              </div>
+              <div
+                className="bottom-item pl-4 pr-4 hover:underline"
+                style={{ padding: ".25rem 2rem" }}
+              >
+                FINE ART
+              </div>
+            </div>
           </div>
-          <div
+          {/* <div
             style={{ paddingTop: "1.25rem" }}
             className="menu absolute text-xl bg-white"
           >
-            <div className=" pl-4 pr-4" style={{ padding: ".25rem 2rem" }}>
+            <div
+              className="pl-4 pr-4"
+              style={{ padding: ".25rem 2rem" }}
+            >
               ILLUSTRATION
             </div>
-            <div className=" pl-4 pr-4" style={{ padding: ".25rem 2rem" }}>
+            <div
+              className="pl-4 pr-4"
+              style={{ padding: ".25rem 2rem" }}
+            >
               ANIMATION
             </div>
-            <div className=" pl-4 pr-4" style={{ padding: ".25rem 2rem" }}>
+            <div
+              className="pl-4 pr-4"
+              style={{ padding: ".25rem 2rem" }}
+            >
               FINE ART
             </div>
-          </div>
+          </div> */}
         </div>
         <div className="flex-grow flex justify-center items-center">
           <div
             style={{
               width: "75%",
-              height: "100%",
-              top: "-2px"
+              height: "100%"
+              // top: "-2px"
             }}
-            className="flex justify-center items-center relative text-gray-800 text-2xl border-solid border-transparent border-t-2 hover:border-black hover:text-black"
+            className="flex justify-center items-center relative text-black text-2xl hover:underline"
           >
             ABOUT
           </div>
@@ -63,10 +107,10 @@ export default (props: NavbarProps) => {
           <div
             style={{
               width: "75%",
-              height: "100%",
-              top: "-2px"
+              height: "100%"
+              // top: "-2px"
             }}
-            className="flex justify-center items-center relative text-gray-800 text-2xl border-solid border-transparent border-t-2 hover:border-black hover:text-black"
+            className="flex justify-center items-center relative text-black text-2xl hover:underline"
           >
             CONTACT
           </div>
