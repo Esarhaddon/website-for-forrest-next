59e98d9b28f726d5a3eb13131f5206749481b268
Jackson Dickison <jaksondikison@gmail.com>
2019-09-12 21:38:12 -0700

    clean up code and add actual navigation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,5 @@
+import Link from "next/Link";
+
 interface NavbarProps {
   className?: string;
   vhFromTop: string;
@@ -12,49 +14,43 @@ export default (props: NavbarProps) => {
         top: props.vhFromTop
       }}
     >
+      <style jsx>{`
+        div {
+          font-family: Arial, Helvetica, sans-serif;
+        }
+
+        .menu {
+          top: 17vh;
+          left: -1.5rem;
+        }
+
+        .menu-container:hover .menu {
+          bottom: calc(17vh - 1px);
+          top: auto;
+        }
+      `}</style>
       <div className="flex h-full w-4/5">
-        <div className="flex-grow flex justify-center h-full w-9/12 items-center">
-          <style jsx>{`
-            div {
-              font-family: Arial, Helvetica, sans-serif;
-            }
-
-            .menu {
-              top: 17vh;
-              left: -1.5rem;
-            }
-
-            .menu-container:hover .menu {
-              bottom: calc(17vh - 1px);
-              top: auto;
-            }
-          `}</style>
-          <div className="menu-container flex justify-center h-full w-9/12 items-center relative text-black text-4xl">
-            <div className="h-full flex items-center relative">
-              WORK
-              <div className="menu absolute bg-white border-t border-r border-l border-solid border-gray-500 ">
-                <div className="text-3xl text-black tracking-tight pl-6 pr-6 pt-4 leading-loose">
-                  ILLUSTRATION
-                </div>
-                <div className="text-3xl text-black tracking-tight pl-6 pr-6 leading-loose">
-                  ANIMATION
-                </div>
-                <div className="border-b border-gray-300 border-solid text-3xl text-black tracking-tight pl-6 pr-6 pb-2 leading-loose">
-                  FINE ART
-                </div>
+        <div className="menu-container flex-grow flex justify-center h-full  items-center relative text-black text-4xl">
+          <div className="flex h-full items-center relative">
+            WORK
+            <div className="menu absolute bg-white border-t border-r border-l border-solid border-gray-500 ">
+              <div className="text-3xl text-black tracking-tight pl-6 pr-6 pt-4 leading-loose">
+                <Link href="/illustration">ILLUSTRATION</Link>
+              </div>
+              <div className="text-3xl text-black tracking-tight pl-6 pr-6 leading-loose">
+                <Link href="/animation">ANIMATION</Link>
+              </div>
+              <div className="border-b border-gray-300 border-solid text-3xl text-black tracking-tight pl-6 pr-6 pb-2 leading-loose">
+                <Link href="/fine-art">FINE ART</Link>
               </div>
             </div>
           </div>
         </div>
-        <div className="flex-grow flex justify-center h-full w-9/12 items-center">
-          <div className="flex justify-center h-full w-9/12 items-center relative text-black text-4xl">
-            ABOUT
-          </div>
+        <div className="flex-grow flex justify-center items-center text-4xl">
+          <Link href="/about">ABOUT</Link>
         </div>
-        <div className="flex-grow flex justify-center h-full w-9/12 items-center">
-          <div className="flex justify-center h-full w-9/12 items-center relative text-black text-4xl">
-            CONTACT
-          </div>
+        <div className="flex-grow flex justify-center items-center text-4xl">
+          <Link href="/contact">CONTACT</Link>
         </div>
       </div>
     </div>
