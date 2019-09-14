5e312e2db28d7bbacd83e795bacc70a0075a2530
Jackson Dickison <jaksondikison@gmail.com>
2019-09-14 14:51:43 -0700

    fix link problem in Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -22,22 +22,32 @@ export default (props: NavbarProps) => {
             WORK
             <div className="menu absolute hidden bg-white border-t border-r border-l border-solid border-gray-500 group-hover:block">
               <div className="text-3xl text-black tracking-tight pl-6 pr-6 pt-4 leading-loose">
-                <Link href="/illustration">ILLUSTRATION</Link>
+                <Link href="/illustration">
+                  <a>ILLUSTRATION</a>
+                </Link>
               </div>
               <div className="text-3xl text-black tracking-tight pl-6 pr-6 leading-loose">
-                <Link href="/animation">ANIMATION</Link>
+                <Link href="/animation">
+                  <a>ANIMATION</a>
+                </Link>
               </div>
               <div className="border-b border-gray-300 border-solid text-3xl text-black tracking-tight pl-6 pr-6 pb-2 leading-loose">
-                <Link href="/fine-art">FINE ART</Link>
+                <Link href="/fine-art">
+                  <a>FINE ART</a>
+                </Link>
               </div>
             </div>
           </div>
         </div>
         <div className="flex-grow flex justify-center items-center text-4xl">
-          <Link href="/about">ABOUT</Link>
+          <Link href="/about">
+            <a>ABOUT</a>
+          </Link>
         </div>
         <div className="flex-grow flex justify-center items-center text-4xl">
-          <Link href="/contact">CONTACT</Link>
+          <Link href="/contact">
+            <a>CONTACT</a>
+          </Link>
         </div>
       </div>
     </div>
