b0d42595f75f2d19b616ed53e26bf75ec9fd8ffb
Jackson Dickison <jaksondikison@gmail.com>
2019-09-14 17:22:05 -0700

    add dummy content for about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,96 @@
 import Layout from "../components/Layout";
 
 export default () => {
-  return <Layout isParallax={false}></Layout>;
+  return (
+    <Layout isParallax={false}>
+      <div
+        className="bg-gray-200"
+        style={{
+          margin: "4rem auto",
+          width: "15rem",
+          height: "18rem"
+        }}
+      ></div>
+      <div
+        className="leading-loose text-gray-300"
+        style={{
+          textIndent: "4rem",
+          textAlign: "justify",
+          width: "60%",
+          marginLeft: "auto",
+          marginRight: "auto",
+          marginBottom: "16rem"
+        }}
+      >
+        <p>
+          Dolores et gubergren et ea rebum aliquyam, est clita ipsum dolores
+          amet dolor vero gubergren consetetur. Dolore gubergren sea et kasd
+          voluptua dolores. Sanctus lorem labore gubergren amet dolore lorem ea
+          nonumy. Dolores ipsum amet vero eos ipsum magna sea clita takimata,
+          diam ipsum et dolores amet dolor sit ipsum sanctus. Aliquyam diam
+          nonumy dolore magna sed. Eos nonumy vero tempor et tempor lorem et
+          diam rebum, justo consetetur vero ipsum voluptua ipsum lorem accusam
+          accusam invidunt. Dolor no stet clita justo nonumy magna sed, dolor
+          sed sanctus est et aliquyam duo lorem, diam elitr est sit sea. Labore
+          takimata et diam takimata clita. Accusam dolor amet voluptua takimata
+          lorem voluptua sanctus, lorem sadipscing clita sed elitr elitr,
+          sanctus eos sanctus sadipscing dolores sed amet, eos ut sadipscing
+          invidunt justo et, dolores gubergren est ut et erat sanctus ut eos
+          invidunt. Dolores amet sit sea ipsum takimata, diam voluptua rebum
+          elitr sit dolor ea amet no sed.
+        </p>
+        <p>
+          Dolores et gubergren et ea rebum aliquyam, est clita ipsum dolores
+          amet dolor vero gubergren consetetur. Dolore gubergren sea et kasd
+          voluptua dolores. Sanctus lorem labore gubergren amet dolore lorem ea
+          nonumy. Dolores ipsum amet vero eos ipsum magna sea clita takimata,
+          diam ipsum et dolores amet dolor sit ipsum sanctus. Aliquyam diam
+          nonumy dolore magna sed. Eos nonumy vero tempor et tempor lorem et
+          diam rebum, justo consetetur vero ipsum voluptua ipsum lorem accusam
+          accusam invidunt. Dolor no stet clita justo nonumy magna sed, dolor
+          sed sanctus est et aliquyam duo lorem, diam elitr est sit sea. Labore
+          takimata et diam takimata clita. Accusam dolor amet voluptua takimata
+          lorem voluptua sanctus, lorem sadipscing clita sed elitr elitr,
+          sanctus eos sanctus sadipscing dolores sed amet, eos ut sadipscing
+          invidunt justo et, dolores gubergren est ut et erat sanctus ut eos
+          invidunt. Dolores amet sit sea ipsum takimata, diam voluptua rebum
+          elitr sit dolor ea amet no sed.
+        </p>
+        <p>
+          Dolores et gubergren et ea rebum aliquyam, est clita ipsum dolores
+          amet dolor vero gubergren consetetur. Dolore gubergren sea et kasd
+          voluptua dolores. Sanctus lorem labore gubergren amet dolore lorem ea
+          nonumy. Dolores ipsum amet vero eos ipsum magna sea clita takimata,
+          diam ipsum et dolores amet dolor sit ipsum sanctus. Aliquyam diam
+          nonumy dolore magna sed. Eos nonumy vero tempor et tempor lorem et
+          diam rebum, justo consetetur vero ipsum voluptua ipsum lorem accusam
+          accusam invidunt. Dolor no stet clita justo nonumy magna sed, dolor
+          sed sanctus est et aliquyam duo lorem, diam elitr est sit sea. Labore
+          takimata et diam takimata clita. Accusam dolor amet voluptua takimata
+          lorem voluptua sanctus, lorem sadipscing clita sed elitr elitr,
+          sanctus eos sanctus sadipscing dolores sed amet, eos ut sadipscing
+          invidunt justo et, dolores gubergren est ut et erat sanctus ut eos
+          invidunt. Dolores amet sit sea ipsum takimata, diam voluptua rebum
+          elitr sit dolor ea amet no sed.
+        </p>
+        <p>
+          Dolores et gubergren et ea rebum aliquyam, est clita ipsum dolores
+          amet dolor vero gubergren consetetur. Dolore gubergren sea et kasd
+          voluptua dolores. Sanctus lorem labore gubergren amet dolore lorem ea
+          nonumy. Dolores ipsum amet vero eos ipsum magna sea clita takimata,
+          diam ipsum et dolores amet dolor sit ipsum sanctus. Aliquyam diam
+          nonumy dolore magna sed. Eos nonumy vero tempor et tempor lorem et
+          diam rebum, justo consetetur vero ipsum voluptua ipsum lorem accusam
+          accusam invidunt. Dolor no stet clita justo nonumy magna sed, dolor
+          sed sanctus est et aliquyam duo lorem, diam elitr est sit sea. Labore
+          takimata et diam takimata clita. Accusam dolor amet voluptua takimata
+          lorem voluptua sanctus, lorem sadipscing clita sed elitr elitr,
+          sanctus eos sanctus sadipscing dolores sed amet, eos ut sadipscing
+          invidunt justo et, dolores gubergren est ut et erat sanctus ut eos
+          invidunt. Dolores amet sit sea ipsum takimata, diam voluptua rebum
+          elitr sit dolor ea amet no sed.
+        </p>
+      </div>
+    </Layout>
+  );
 };
