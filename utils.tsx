09dede057892242888f6b080cfb9ce3fd4ad83f4
Jackson Dickison <jaksondikison@gmail.com>
2019-09-28 09:44:20 -0700

    finalize styling on portfolio pages

--- utils.tsx
+++ utils.tsx
@@ -21,8 +21,13 @@ export function createThumbnailGroup(
           style={{ top: 0, left: 0 }}
         >
           <div
-            className="absolute bg-gray-500"
-            style={{ top: "30px", bottom: "30px", left: "30px", right: "30px" }}
+            className="absolute bg-white off-bg-gray-200"
+            style={{
+              top: "50px",
+              bottom: "50px",
+              left: "50px",
+              right: "50px"
+            }}
           />
         </div>
       </div>
@@ -31,11 +36,22 @@ export function createThumbnailGroup(
   return (
     <>
       <div
+        className="flex text-gray-400 items-center justify-center fixed w-full off-h-full"
+        style={{
+          height: "calc(100% - 7.5rem)",
+          zIndex: -1,
+          fontSize: `${(100 / groupTitle.length) * 1.5}vw`
+        }}
+      >
+        {groupTitle}
+      </div>
+      <div
         style={{
           display: "grid",
-          margin: "30px",
-          paddingBottom: "7.5rem",
-          gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr)"
+          padding: "50px",
+          paddingBottom: "calc(7.5rem + 50px)",
+          gridTemplateColumns: "repeat(auto-fit, minmax(275px, 1fr)",
+          backgroundColor: "rgba(0,0,0, .75)"
         }}
       >
         {thumbnails}
