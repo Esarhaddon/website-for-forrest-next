810ab2dda8a0328668d4e340a048be678c12ff70
Jackson Dickison <jaksondikison@gmail.com>
2019-10-07 19:04:20 -0700

    cleanup Layout component and add footer code to Layout component and get rid of Navbar component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,14 +1,83 @@
-import Navbar from "../components/Navbar";
-import { callbackify } from "util";
+import Link from "next/Link";
+import InstagramLogo from "../static/icons/001-instagram.svg";
+import FacebookLogo from "../static/icons/002-facebook-circular-logo.svg";
+import TwitterLogo from "../static/icons/003-twitter-circular-logo.svg";
+import EmailLogo from "../static/icons/004-email.svg";
+import { ReactNode } from "react";
 
-export default props => (
-  <div>
-    <div
-      className="h-screen w-full relative overflow-x-hidden overflow-y-scroll"
-      style={{ perspective: "2px", perspectiveOrigin: "0 0" }}
-    >
-      {props.children}
-      <Navbar />
-    </div>
+interface LayoutProps {
+  isFor: "home" | "work";
+  children: ReactNode;
+}
+
+export default (props: LayoutProps) => (
+  <div
+    className="h-screen w-full relative overflow-x-hidden overflow-y-scroll"
+    style={{
+      perspective: "2px",
+      perspectiveOrigin: "0 0"
+    }}
+  >
+    {props.children}
+    {props.isFor === "home" ? (
+      <div
+        className="foreground absolute w-full left-0 flex items-center justify-center text-white tracking-tighter leading-none"
+        style={{
+          height: "5rem",
+          bottom: "16vh"
+        }}
+      >
+        <Link href="/work">
+          <a
+            className="border-2 border-solid border-white text-white pt-5 pb-4 px-5 tracking-widest leading-none cursor-pointer hover:bg-white hover:text-black"
+            style={{
+              transition:
+                "color 170ms ease-in-out, background-color 170ms ease-in-out"
+            }}
+          >
+            WORK
+          </a>
+        </Link>
+        <div className="flex items-center ml-12 text-white fill-current logo-container cursor-pointer">
+          <a
+            href="https://www.instagram.com/forrestdickison"
+            target="_blank"
+            className="w-12 pl-2 hover:text-white"
+            style={{
+              fill: "white",
+              transition: "color 170ms ease-in-out, opacity 170ms ease-in-out"
+            }}
+          >
+            <InstagramLogo className="h-8 w-8 fill-current" />
+          </a>
+          <div
+            className="w-12 pl-2 hover:text-white"
+            style={{
+              transition: "color 170ms ease-in-out, opacity 170ms ease-in-out"
+            }}
+          >
+            <FacebookLogo className="h-8 w-8 fill-current" />
+          </div>
+          <div
+            className="w-12 pl-2 hover:text-white"
+            style={{
+              transition: "color 170ms ease-in-out, opacity 170ms ease-in-out"
+            }}
+          >
+            <TwitterLogo className="h-8 w-8 fill-current" />
+          </div>
+          <div
+            className="w-12 pl-2 hover:text-white"
+            style={{
+              transition: "color 170ms ease-in-out, opacity 170ms ease-in-out"
+            }}
+          >
+            <a href="mailto:fddickison@gmail.com" target="_blank">
+              <EmailLogo className="h-8 w-8 fill-current" />
+            </a>
+          </div>
+        </div>
+      </div>
+    ) : null}
   </div>
 );
--- utils.tsx
+++ utils.tsx
@@ -1,12 +1,12 @@
 import { relative } from "path";
 
-export function createThumbnailGroup(
-  groupTitle: string,
-  numOfThumbnails: number
+export function createGrid(
+  // groupTitle: string,
+  numOfImages: number
 ) {
-  const thumbnails: JSX.Element[] = [];
-  for (let i = 0; i < numOfThumbnails; i++) {
-    thumbnails.push(
+  const grid: JSX.Element[] = [];
+  for (let i = 0; i < numOfImages; i++) {
+    grid.push(
       <div
         key={i}
         className=""
@@ -34,28 +34,16 @@ export function createThumbnailGroup(
     );
   }
   return (
-    <>
-      <div
-        className="flex text-gray-400 items-center justify-center fixed w-full off-h-full"
-        style={{
-          height: "calc(100% - 7.5rem)",
-          zIndex: -1,
-          fontSize: `${(100 / groupTitle.length) * 1.5}vw`
-        }}
-      >
-        {groupTitle}
-      </div>
-      <div
-        style={{
-          display: "grid",
-          padding: "50px",
-          paddingBottom: "calc(7.5rem + 50px)",
-          gridTemplateColumns: "repeat(auto-fit, minmax(275px, 1fr)",
-          backgroundColor: "rgba(0,0,0, .75)"
-        }}
-      >
-        {thumbnails}
-      </div>
-    </>
+    <div
+      style={{
+        display: "grid",
+        padding: "50px",
+        paddingBottom: "calc(7.5rem + 50px)",
+        gridTemplateColumns: "repeat(auto-fit, minmax(275px, 1fr)",
+        backgroundColor: "rgba(0,0,0, .75)"
+      }}
+    >
+      {grid}
+    </div>
   );
 }
