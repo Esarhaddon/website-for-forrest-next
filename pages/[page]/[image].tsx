8914fff1a89db3c98af762bf0d97f8199574ddcc
Jackson Dickison <jaksondikison@gmail.com>
2019-10-14 14:33:27 -0700

    add modal for displaying image fit to page

--- pages/[page]/[image].tsx
+++ pages/[page]/[image].tsx
@@ -1,50 +1,14 @@
 import Layout from "../../components/Layout";
-// import { useImage } from "../components/ImageContextProvider";
-import { useRouter, Router } from "next/router";
-import { loadGetInitialProps } from "next-server/dist/lib/utils";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import ExitX from "../../static/icons/close.svg";
 
 export default () => {
   const router = useRouter();
-  // let slug;
-  // if (typeof router.query.slug === "string") {
-  //   slug = JSON.parse(router.query.slug);
-  // } else if (slug) {
-  //   console.log("typeof slug is", router.query.slug);
-  //   slug = JSON.parse(router.query.slug.join());
-  // }
-
-  // const {fromPage, imageSource, imageName} = JSON.parse(router.query.slug.join(','))
-  // const { image } = useImage();
-  // console.log(image.name);
-  // if (image === null) {
-  //   router.push("/illustration");
-  // }
-
+  const [hideModal, setHideModal] = useState(true);
   return (
     <div>
-      {/* {image === null ? router.push("/illustration") : null} */}
-      {/* {console.log('router.quert')} */}
       <Layout isFor={router.query ? router.query.page : ""}>
-        {/* {image === null ? router.push("/animation") : null} */}
-        {/* {image.name !== "2" ? router.push("/illustration") : null} */}
-        {/* <div
-        className="w-1/2 border border-solid border-red-500"
-        style={
-          {
-            //   padding: "5vw",
-            //   paddingTop: 0
-          }
-        }
-      >
-        <div
-          className="bg-black"
-          style={{
-            width: "50px",
-            height: 0,
-            paddingTop: "100%"
-          }}
-        />
-      </div> */}
         <div
           style={{
             padding: "5vw",
@@ -63,11 +27,35 @@ export default () => {
                 paddingTop: "127.208%"
               }}
             >
-              <div className="absolute top-0 left-0 flex items-center justify-center text-white text-6xl w-full h-full">
+              <div
+                className="absolute top-0 left-0 flex items-center justify-center text-white text-6xl w-full h-full cursor-pointer"
+                onClick={() => {
+                  setHideModal(false);
+                }}
+              >
                 [ {router.query ? router.query.image : "no image"} ]
               </div>
             </div>
           </div>
+        </div>
+        <div
+          className={`fixed top-0 left-0 w-full h-full bg-black ${
+            hideModal ? "hidden" : ""
+          }`}
+          style={{ opacity: 0.95 }}
+        >
+          <ExitX
+            className="absolute text-gray-200 fill-current cursor-pointer"
+            style={{
+              top: "calc(2% + 1rem)",
+              right: "2%",
+              width: ".85rem",
+              height: ".85rem"
+            }}
+            onClick={() => {
+              setHideModal(true);
+            }}
+          />
         </div>
       </Layout>
     </div>
