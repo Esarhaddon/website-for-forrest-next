c536ad4cc58cc00cb022505855639e3f26ec7ef7
Jackson Dickison <jaksondikison@gmail.com>
2019-12-17 09:02:12 -0800

    remove extra code and use of position: sticky

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,6 @@ const Index = props => {
   cookie.set("indexHasLoaded", indexHasLoaded + 1);
 
   useEffect(() => {
-    console.log("adding event listener");
     window.addEventListener("unload", () => {
       cookie.remove("indexHasLoaded");
     });
@@ -29,78 +28,72 @@ const Index = props => {
   }
 
   return (
-    <div
-      className={`absolute top-0 left-0 h-screen w-full overflow-x-hidden overflow-y-scroll ${
-        indexHasLoaded ? "opacity-1" : "opacity-0"
-      } ${scrollable ? "" : "pointer-events-none"}`}
-      style={{
-        perspective: "2px",
-        perspectiveOrigin: "0 0",
-        minHeight: "375px",
-        ...(indexHasLoaded ? null : { animation: "1s fade-in .75s forwards" })
-      }}
-    >
-      <div
-        className="absolute top-0 left-0 w-full"
-        style={{
-          height: "130%",
-          background:
-            "url(../static/landing-background-cropped.png) 66.66% 0% / cover no-repeat",
-          transformOrigin: "0 0",
-          transform: "translateZ(-2px) scale(2)" // (perspective - distance) / perspective = scaleFactor
-        }}
-      />
-      <div
-        className="absolute top-0 left-0 w-full"
-        style={{
-          height: "130%",
-          background:
-            "linear-gradient(rgba(0, 0, 0, .25), rgba(0, 0, 0, .25)), url(../static/layer1.png) 33.33% 0% / cover no-repeat",
-          transformOrigin: "0 0",
-          transform: "translateZ(-1px) scale(1.5)"
-        }}
-      />
-      <div
-        className="flex items-center justify-center text-center h-screen w-screen top-0 px-2 text-white md:text-8xl sm:text-7xl xsm:text-6xl text-5-1/4xl sticky leading-none font-semibold"
-        style={{
-          ...(!indexHasLoaded
-            ? { animation: "1s fade-bg .75s forwards" }
-            : null)
-        }}
-      >
+    <>
+      <div className="pointer-events-none z-50 flex items-center justify-center text-center h-screen w-screen top-0 px-2 text-white md:text-8xl sm:text-7xl xsm:text-6xl text-5-1/4xl fixed leading-none font-semibold">
         forrest dickison
       </div>
       <div
-        className="flex-wrap absolute left-0 off-h-20 w-full left-0 flex items-center justify-center pb-4"
+        className={`absolute top-0 left-0 h-screen w-full overflow-x-hidden overflow-y-scroll ${
+          indexHasLoaded ? "opacity-1" : "opacity-0"
+        } ${scrollable ? "" : "pointer-events-none"}`}
         style={{
-          top: "105%",
-          ...(!indexHasLoaded ? { animation: "1.25s ease .5s show-nav" } : null)
+          perspective: "2px",
+          perspectiveOrigin: "0 0",
+          minHeight: "375px",
+          ...(indexHasLoaded ? null : { animation: "1s fade-in .75s forwards" })
         }}
       >
-        <Link href="/[page]" as="/illustration">
-          <a
-            className="mx-12 my-6 border-2 border-solid border-white text-white py-4 px-5 tracking-widest leading-none cursor-pointer hover:bg-white hover:text-gray-900"
-            style={{
-              transition:
-                "color 170ms ease-in-out, background-color 170ms ease-in-out"
-            }}
-          >
-            WORK
-          </a>
-        </Link>
-        <SocialAndEmail isDark={false} />
+        <div
+          className="absolute top-0 left-0 w-full"
+          style={{
+            height: "130%",
+            background:
+              "url(../static/landing-background-cropped.png) 66.66% 0% / cover no-repeat",
+            transformOrigin: "0 0",
+            transform: "translateZ(-2px) scale(2)" // (perspective - distance) / perspective = scaleFactor
+          }}
+        />
+        <div
+          className="absolute top-0 left-0 w-full"
+          style={{
+            height: "130%",
+            background:
+              "linear-gradient(rgba(0, 0, 0, .25), rgba(0, 0, 0, .25)), url(../static/layer1.png) 33.33% 0% / cover no-repeat",
+            transformOrigin: "0 0",
+            transform: "translateZ(-1px) scale(1.5)"
+          }}
+        />
+        <div
+          className="flex-wrap absolute left-0 off-h-20 w-full left-0 flex items-center justify-center pb-4"
+          style={{
+            top: "105%",
+            ...(!indexHasLoaded
+              ? { animation: "1.25s ease .5s show-nav" }
+              : null)
+          }}
+        >
+          <Link href="/[page]" as="/illustration">
+            <a
+              className="mx-12 my-6 border-2 border-solid border-white text-white py-4 px-5 tracking-widest leading-none cursor-pointer hover:bg-white hover:text-gray-900"
+              style={{
+                transition:
+                  "color 170ms ease-in-out, background-color 170ms ease-in-out"
+              }}
+            >
+              WORK
+            </a>
+          </Link>
+          <SocialAndEmail isDark={false} />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
 Index.getInitialProps = async ctx => {
-  console.log("getInitialProps called for Index page");
   const { indexHasLoaded } = nextCookie(ctx);
-  const allCookies = nextCookie(ctx);
   return {
-    indexHasLoaded,
-    allCookies
+    indexHasLoaded
   };
 };
 
