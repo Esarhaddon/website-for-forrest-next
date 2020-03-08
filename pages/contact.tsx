6de7167c33c634796d7b6b198f2f7bd1b60a17d0
Jackson Dickison <jaksondikison@gmail.com>
2020-03-07 16:14:49 -0800

    add Layout.tsx to /contact.tsx

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,2 +1,8 @@
-import React from "react"
-export default () => "This will be the contact page"
+import Layout from "../components/Layout"
+export default () => (
+  <Layout isFor="contact">
+    <div className="w-full h-full flex justify-center items-center text-6xl text-bold text-gray-500">
+      CONTACT
+    </div>
+  </Layout>
+)
