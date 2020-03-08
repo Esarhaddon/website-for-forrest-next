dfe21df1f76f9a8c6b1f8c7de506871ff26553c9
Jackson Dickison <jaksondikison@gmail.com>
2020-03-07 16:18:25 -0800

    add Layout.tsx to about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,2 +1,8 @@
-import react from "react"
-export default () => "This will be the about page"
+import Layout from "../components/Layout"
+export default () => (
+  <Layout isFor="about">
+    <div className="w-full h-full flex justify-center items-center text-6xl text-bold text-gray-500">
+      ABOUT
+    </div>
+  </Layout>
+)
