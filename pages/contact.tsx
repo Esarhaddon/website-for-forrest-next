53f518fa0d06fe24959003783299b67e76a2d811
Jackson Dickison <jaksondikison@gmail.com>
2020-03-26 16:46:36 -0700

    start on contact form

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,8 +1,46 @@
 import Layout from "../components/Layout"
+import { Field, Form, Formik } from "formik"
+
 export default () => (
   <Layout isFor="contact">
-    <div className="w-full h-full flex justify-center items-center text-6xl text-bold text-gray-500">
+    {/* <div className="w-full h-full flex justify-center items-center text-6xl text-bold text-gray-500">
       CONTACT
-    </div>
+    </div> */}
+    <Formik
+      initialValues={{
+        firstName: "",
+        lastName: "",
+        emailAddress: "",
+        subjectLine: "",
+        text: ""
+      }}
+      onSubmit={() => console.log("Bang! Yur dead.")}
+    >
+      <Form className="flex flex-col">
+        <div className="flex flex-wrap">
+          <Field
+            className="border border-solid border-gray-500 rounded flex-1 mx-1"
+            name="firstName"
+          />
+          <Field
+            className="border border-solid border-gray-500 rounded flex-1 mx-1"
+            name="lastName"
+          />
+        </div>
+        <Field
+          className="border border-solid border-gray-500 rounded flex-1 mx-1"
+          name="emailAddress"
+        />
+        <Field
+          className="border border-solid border-gray-500 rounded flex-1 mx-1"
+          name="subjectLine"
+        />
+        <Field
+          className="border border-solid border-gray-500 rounded flex-1 mx-1"
+          name="text"
+        />
+        <button type="submit">Send</button>
+      </Form>
+    </Formik>
   </Layout>
 )
