613b883732b0f03be4826fef0dd61b1a0ddc2da0
Jackson Dickison <jaksondikison@gmail.com>
2020-06-03 16:22:44 -0700

    delete commented out code

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -30,86 +30,8 @@ export default (props) => {
     window.scrollTo(0, 0)
   }, [isLoading])
 
-  // TO DO: make index its own page and just return that
   if (isFor === "index") {
-    return (
-      <div>
-        <div
-          className="absolute top-0 left-0 w-full h-full"
-          style={{
-            background: "url(../static/toad.png) 66.66%  25% / cover no-repeat",
-          }}
-        />
-        <div
-          className="absolute top-0 left-0 w-full h-full"
-          style={{
-            background:
-              "linear-gradient(rgba(0, 0, 0, .25), rgba(0, 0, 0, .25)), url(../static/boy.png) 33.33%  25% / cover no-repeat",
-          }}
-        />
-        <div
-          className="relative max-w-3xl mx-auto px-4"
-          style={{ top: "33vh" }}
-        >
-          <FD className="w-full" />
-        </div>
-        <div className="absolute bottom-0 sm:mb-16 mb-4 flex flex-col w-full">
-          <div className="flex flex-col sm:flex sm:flex-row sm:justify-center sm:flex-wrap">
-            <Link href="/[grid]" as="/illustration">
-              <a>
-                <div className="text-center">
-                  <button
-                    type="button"
-                    className="mx-4 border-2 focus:bg-white focus:text-black border-solid border-white text-white py-2 px-3 sm:py-4 sm:px-5 tracking-widest mt-6 leading-none cursor-pointer hover:bg-white hover:text-gray-900"
-                    style={{
-                      transition:
-                        "color 170ms ease-in-out, background-color 170ms ease-in-out",
-                    }}
-                  >
-                    ILLUSTRATION
-                  </button>
-                </div>
-              </a>
-            </Link>
-            <Link href="/about">
-              <a>
-                <div className="text-center">
-                  <button
-                    type="button"
-                    className="mx-4 border-2 focus:bg-white focus:text-black border-solid border-white text-white py-2 px-3 sm:py-4 sm:px-5 tracking-widest mt-6 leading-none cursor-pointer hover:bg-white hover:text-gray-900"
-                    style={{
-                      transition:
-                        "color 170ms ease-in-out, background-color 170ms ease-in-out",
-                    }}
-                  >
-                    ABOUT
-                  </button>
-                </div>
-              </a>
-            </Link>
-            <div className="text-center">
-              <Link href="/contact">
-                <a>
-                  <button
-                    type="button"
-                    className="hidden sm:inline mx-4 border-2 focus:bg-white focus:text-black border-solid border-white text-white py-2 px-3 sm:py-4 sm:px-5 tracking-widest mt-6 leading-none cursor-pointer hover:bg-white hover:text-gray-900"
-                    style={{
-                      transition:
-                        "color 170ms ease-in-out, background-color 170ms ease-in-out",
-                    }}
-                  >
-                    CONTACT
-                  </button>
-                </a>
-              </Link>
-            </div>
-          </div>
-          <div className="mt-6">
-            <SocialAndEmail isDark={false} includesEmailOnMobile={true} />
-          </div>
-        </div>
-      </div>
-    )
+    return props.children
   }
 
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import SocialAndEmail from "../components/SocialAndEmail"
 import FD from "../static/icons/forrest-dickison.svg"
 import Link from "next/link"
 
-// optimize background images for this page as well
+// TO DO: optimize background images for this page as well?
 
 const Index = () => {
   return (
@@ -11,14 +11,14 @@ const Index = () => {
       <div
         className="absolute top-0 left-0 w-full h-full"
         style={{
-          background: "url(../static/toad.png) 66.66%  25% / cover no-repeat"
+          background: "url(../static/toad.png) 66.66%  25% / cover no-repeat",
         }}
       />
       <div
         className="absolute top-0 left-0 w-full h-full"
         style={{
           background:
-            "linear-gradient(rgba(0, 0, 0, .25), rgba(0, 0, 0, .25)), url(../static/boy.png) 33.33%  25% / cover no-repeat"
+            "linear-gradient(rgba(0, 0, 0, .25), rgba(0, 0, 0, .25)), url(../static/boy.png) 33.33%  25% / cover no-repeat",
         }}
       />
       <div className="relative max-w-3xl mx-auto px-4" style={{ top: "33vh" }}>
@@ -34,7 +34,7 @@ const Index = () => {
                   className="mx-4 border-2 focus:bg-white focus:text-black border-solid border-white text-white py-2 px-3 sm:py-4 sm:px-5 tracking-widest mt-6 leading-none cursor-pointer hover:bg-white hover:text-gray-900"
                   style={{
                     transition:
-                      "color 170ms ease-in-out, background-color 170ms ease-in-out"
+                      "color 170ms ease-in-out, background-color 170ms ease-in-out",
                   }}
                 >
                   ILLUSTRATION
@@ -50,7 +50,7 @@ const Index = () => {
                   className="mx-4 border-2 focus:bg-white focus:text-black border-solid border-white text-white py-2 px-3 sm:py-4 sm:px-5 tracking-widest mt-6 leading-none cursor-pointer hover:bg-white hover:text-gray-900"
                   style={{
                     transition:
-                      "color 170ms ease-in-out, background-color 170ms ease-in-out"
+                      "color 170ms ease-in-out, background-color 170ms ease-in-out",
                   }}
                 >
                   ABOUT
@@ -66,7 +66,7 @@ const Index = () => {
                   className="hidden sm:inline mx-4 border-2 focus:bg-white focus:text-black border-solid border-white text-white py-2 px-3 sm:py-4 sm:px-5 tracking-widest mt-6 leading-none cursor-pointer hover:bg-white hover:text-gray-900"
                   style={{
                     transition:
-                      "color 170ms ease-in-out, background-color 170ms ease-in-out"
+                      "color 170ms ease-in-out, background-color 170ms ease-in-out",
                   }}
                 >
                   CONTACT
