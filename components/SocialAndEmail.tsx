1ba436a14e708931cd2e51ea42930b3bc649323e
Jackson Dickison <jaksondikison@gmail.com>
2020-06-12 21:51:05 -0700

    add remaining links

--- components/SocialAndEmail.tsx
+++ components/SocialAndEmail.tsx
@@ -17,40 +17,55 @@ export default (props: SocialAndEmailProps) => {
     <div className="flex justify-center">
       <div
         className={`flex justify-center cursor-pointer hover-text-transparent-${brightness} text-${baseColor}`}
-        // style={{ width: "9rem" }}
       >
-        <a
-          href="https://www.instagram.com/forrestdickison"
-          target="_blank"
-          className={`w-12 hover:text-${baseColor}`}
-          style={{
-            transition: "color 170ms ease-in-out, opacity 170ms ease-in-out"
-          }}
-        >
-          <InstagramLogo className="h-8 w-8 fill-current mx-auto" />
-        </a>
         <div
           className={`w-12 hover:text-${baseColor}`}
           style={{
-            transition: "color 170ms ease-in-out, opacity 170ms ease-in-out"
+            transition: "color 170ms ease-in-out, opacity 170ms ease-in-out",
           }}
         >
-          <FacebookLogo className="h-8 w-8 fill-current mx-auto" />
+          <a
+            href="https://www.instagram.com/forrestdickison"
+            target="_blank"
+            rel="noopener"
+          >
+            <InstagramLogo className="h-8 w-8 fill-current mx-auto" />
+          </a>
         </div>
         <div
           className={`w-12 hover:text-${baseColor}`}
           style={{
-            transition: "color 170ms ease-in-out, opacity 170ms ease-in-out"
+            transition: "color 170ms ease-in-out, opacity 170ms ease-in-out",
           }}
         >
-          <TwitterLogo className="h-8 w-8 fill-current mx-auto" />
+          <a
+            href="https://www.facebook.com/forrestdickison/"
+            target="_blank"
+            rel="noopener"
+          >
+            <FacebookLogo className="h-8 w-8 fill-current mx-auto" />
+          </a>
+        </div>
+        <div
+          className={`w-12 hover:text-${baseColor}`}
+          style={{
+            transition: "color 170ms ease-in-out, opacity 170ms ease-in-out",
+          }}
+        >
+          <a
+            href="https://twitter.com/forrestdickison"
+            target="_blank"
+            rel="noopener"
+          >
+            <TwitterLogo className="h-8 w-8 fill-current mx-auto" />
+          </a>
         </div>
         <div
           className={`w-12 hover:text-${baseColor} sm:hidden ${
             !props.includesEmailOnMobile ? "hidden" : ""
           }`}
           style={{
-            transition: "color 170ms ease-in-out, opacity 170ms ease-in-out"
+            transition: "color 170ms ease-in-out, opacity 170ms ease-in-out",
           }}
         >
           <Link href="/contact">
