2e9f30b5a4708e2707ecc6a6cde74ca21f5066f1
Jackson Dickison <jaksondikison@gmail.com>
2020-06-25 21:52:12 -0700

    delete  console.log statements

--- utils/fetchImagesFor.ts
+++ utils/fetchImagesFor.ts
@@ -87,16 +87,13 @@ export default async (imagesFor: "illustration"): Promise<Image[]> => {
   // at some point you need to figure out pagination
   const content: IllustrationPageContent = await res.json()
   const illustrations: Reference[] = content.items[0].fields.illustrations
-  console.log("illustrations are", illustrations)
   const entries: ArtWork[] = illustrations.map((illustration) =>
     content.includes.Entry.find((entry) => illustration.sys.id === entry.sys.id)
   )
-  console.log("entries are", entries)
   const images: Image[] = entries.map((entry) => {
     const asset = content.includes.Asset.find(
       (asset) => asset.sys.id === entry.fields.image.sys.id
     )
-    console.log("asset is", asset)
     return {
       title: entry.fields.title,
       description: entry.fields.description,
