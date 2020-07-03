2fd84764e72f07b403bf80ac8c5d5158fac68b41
Jackson Dickison <jaksondikison@gmail.com>
2020-07-03 14:33:02 -0700

    fetch about page copy from cms

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,16 +2,22 @@ import fetch from "node-fetch"
 import ErrorMessage from "../components/ErrorMessage"
 
 interface AboutPageProps {
-  textContent: TextContent
+  textContent: Paragraph[]
   errorMessage?: string
   errorCode?: number
 }
 
-type TextContent = (Link | Text)[]
+interface Paragraph {
+  nodeType: "paragraph"
+  content: (Link | Text)[]
+}
 
 interface Link {
   nodeType: "hyperlink"
   content: Text[]
+  data: {
+    uri: string
+  }
 }
 
 interface Text {
@@ -19,53 +25,64 @@ interface Text {
   value: string
 }
 
-const AboutPage = () => (
-  <div className="flex flex-col items-center px-4">
-    <div className="w-11/12 sm:w-7/12 md:w-6/12 leading-loose flex flex-col items-center lg:block lg:ml-16 mt-20 md:mt-8 sm:mt-20 lg:text-left text-justify mb-4">
-      <div className="relative w-6/12 lg:float-left lg:mr-6 lg:ml-0 ml-16 lg:mb-0 mb-6">
-        <div
-          className="border border-solid border-black w-full h-0"
-          style={{ paddingTop: "120%" }}
-        >
+const AboutPage = ({
+  textContent,
+  errorMessage,
+  errorCode,
+}: AboutPageProps) => {
+  if (errorMessage || errorCode) {
+    return (
+      <div
+        className="flex items-center justify-center"
+        style={{
+          height: "40vh",
+        }}
+      >
+        <ErrorMessage text={errorMessage} code={errorCode} />
+      </div>
+    )
+  }
+
+  return (
+    <div className="flex flex-col items-center px-4">
+      <div className="w-11/12 sm:w-7/12 md:w-6/12 leading-loose flex flex-col items-center lg:block lg:ml-16 mt-20 md:mt-8 sm:mt-20 lg:text-left text-justify mb-4">
+        <div className="relative w-6/12 lg:float-left lg:mr-6 lg:ml-0 ml-16 lg:mb-0 mb-6">
           <div
-            className="bg-gray-500 absolute right-0 bottom-0 text-center flex items-center justify-center text-gray-100 text-3xl"
-            style={{
-              width: "calc(100% + 4rem)",
-              height: "calc(100% + 4rem)",
-            }}
+            className="border border-solid border-black w-full h-0"
+            style={{ paddingTop: "120%" }}
           >
-            [a cool image]
+            <div
+              className="bg-gray-500 absolute right-0 bottom-0 text-center flex items-center justify-center text-gray-100 text-3xl"
+              style={{
+                width: "calc(100% + 4rem)",
+                height: "calc(100% + 4rem)",
+              }}
+            >
+              [a cool image]
+            </div>
           </div>
         </div>
+        {textContent.map((paragraph, i) => (
+          <p className={`${i < textContent.length - 1 ? "mb-4" : ""}`}>
+            {paragraph.content.map((item) => {
+              if (item.nodeType === "text") {
+                return item.value
+              } else {
+                return (
+                  <a className="underline text-gray-700" href={item.data.uri}>
+                    {item.content[0].value}
+                  </a>
+                )
+              }
+            })}
+          </p>
+        ))}
       </div>
-      <p className="mb-4">
-        Deserunt deserunt aliqua enim minim non consequat labore tempor. Nisi do
-        labore ipsum ipsum velit. Mollit labore incididunt aliqua veniam Lorem
-        cillum incididunt cillum consectetur. Ut reprehenderit id fugiat nostrud
-        ad consequat aute nulla. Culpa culpa commodo cillum commodo aute non in
-        nulla ut tempor sit minim cillum. Tempor sint laboris enim id elit et
-        nisi eiusmod consequat adipisicing minim id deserunt cillum.
-      </p>
-      <p className="mb-4">
-        Deserunt deserunt aliqua enim minim non consequat labore tempor. Nisi do
-        labore ipsum ipsum velit. Mollit labore incididunt aliqua veniam Lorem
-        cillum incididunt cillum consectetur. Ut reprehenderit id fugiat nostrud
-        ad consequat aute nulla. Culpa culpa commodo cillum commodo aute non in
-        nulla ut tempor sit minim cillum. Tempor sint laboris enim id elit et
-        nisi eiusmod consequat adipisicing minim id deserunt cillum.
-      </p>
-      <p>
-        Anim non elit laborum irure reprehenderit sit nostrud qui amet elit
-        magna amet aliquip deserunt. Esse esse velit est consequat veniam nulla
-        laborum exercitation sit ea. Nisi ullamco dolore elit id proident est
-        nostrud consequat. Aliqua ullamco sit fugiat eiusmod ut. Officia duis ex
-        eiusmod dolor tempor ut voluptate consequat consectetur exercitation.
-      </p>
     </div>
-  </div>
-)
+  )
+}
 
-AboutPage.getInitialProps = async (): Promise<Partial<AboutPageProps>> => {
+AboutPage.getInitialProps = async (): Promise<Partial<any>> => {
   try {
     const res = await fetch(
       `${process.env.CONTENTFUL_API}?content_type=about`,
@@ -77,7 +94,7 @@ AboutPage.getInitialProps = async (): Promise<Partial<AboutPageProps>> => {
       }
     )
 
-    if (res.ok) {
+    if (!res.ok) {
       throw new Error(`${res.status}`)
     }
 
