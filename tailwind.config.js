e7adbc9954235348a4146f2d9403b2733574c644
Jackson Dickison <jaksondikison@gmail.com>
2020-07-10 16:32:50 -0700

    adjust sm breakpoint in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 module.exports = {
-  // important: true,
   theme: {
     extend: {},
     screens: {
-      xsm: "460px",
-      sm: "640px",
-      // => @media (min-width: 640px) { ... }
+      sm: "600px",
+      // => @media (min-width: 600px) { ... }
 
       md: "768px",
       // => @media (min-width: 768px) { ... }
@@ -13,15 +11,12 @@ module.exports = {
       lg: "1024px",
       // => @media (min-width: 1024px) { ... }
 
-      xl: "1280px"
+      xl: "1280px",
       // => @media (min-width: 1280px) { ... }
-    }
+    },
   },
   variants: {
-    // borderWidth: ["focus"],
-    // display: ["group-hover"],
-    opacity: ["hover", "group-hover"]
-    // textColor: ["hover", "group-hover"]
+    opacity: ["hover", "group-hover"],
   },
-  plugins: []
+  plugins: [],
 }
