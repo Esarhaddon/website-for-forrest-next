362c01a5f4466fd7844bbad3926b7e57e180c39d
Jackson Dickison <jaksondikison@gmail.com>
2020-07-10 14:41:03 -0700

    fade in image on about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -35,14 +35,15 @@ const AboutPage = ({
   errorCode,
 }: AboutPageProps) => {
   const imgLoadingColor: string = useDominantColor(imageSrc)
-  const [imgHeight, setImgHeight] = useState(100)
+  const [imgContainerHeight, setImgContainerHeight] = useState(0)
+  const [imgIsLoaded, setImgIsLoaded] = useState(false)
 
-  const thumbnailRef = useRef<HTMLDivElement>()
+  const imgContainerRef = useRef<HTMLDivElement>()
 
   useEffect(() => {
-    const el = thumbnailRef.current
+    const el = imgContainerRef.current
     if (el) {
-      setImgHeight(el.offsetHeight * 2)
+      setImgContainerHeight(el.offsetHeight * 2)
     }
   }, [])
 
@@ -61,17 +62,36 @@ const AboutPage = ({
 
   return (
     <div className="flex flex-col items-center px-4">
+      {imgContainerHeight ? (
+        <img
+          className="hidden"
+          src={`${imageSrc}?h=${imgContainerHeight}`}
+          onLoad={() => setImgIsLoaded(true)}
+        />
+      ) : null}
       <div className="w-11/12 sm:w-7/12 md:w-6/12 leading-loose flex flex-col items-center lg:block lg:ml-16 mt-20 sm:mt-16 lg:mt-8 lg:text-left text-justify mb-4">
         <div className="relative w-6/12 lg:float-left lg:mr-6 lg:ml-0 ml-16 lg:mb-0 mb-6">
           <div className="w-full h-0" style={{ paddingTop: "120%" }}>
             <div
-              ref={thumbnailRef}
+              ref={imgContainerRef}
               className="absolute right-0 bottom-0"
               style={{
-                background: `center / cover no-repeat url(${imageSrc}?h=${imgHeight})`,
-                backgroundColor: imgLoadingColor,
+                ...(imgIsLoaded
+                  ? {
+                      background: `center / cover no-repeat url(${imageSrc}?h=${imgContainerHeight})`,
+                    }
+                  : null),
                 width: "calc(100% + 4rem)",
                 height: "calc(100% + 4rem)",
+              }}
+            />
+            <div
+              className="absolute right-0 bottom-0"
+              style={{
+                width: "calc(100% + 4rem)",
+                height: "calc(100% + 4rem)",
+                backgroundColor: imgIsLoaded ? "transparent" : imgLoadingColor,
+                transition: "background-color 200ms ease-out",
               }}
             />
           </div>
