dae514e4d650c3e67319f0f3707fe9c3c2340f52
Jackson Dickison <jaksondikison@gmail.com>
2020-07-10 18:55:38 -0700

    document reason for seemingly pointless div in Loading.tsx

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -12,11 +12,12 @@ export default () => {
 
   return (
     <>
+      {/* div to do nothing except keep the scroll bar from flashing off during loading */}
       <div className="w-full h-screen pointer-events-none" />
       <div
         className={`${
           showText ? "opacity-100" : "opacity-0"
-        } w-screen h-full absolute top-0 right-0 flex items-center justify-center pointer-events-none`}
+        } w-full h-full absolute top-0 right-0 flex items-center justify-center pointer-events-none`}
         style={{
           transition: "opacity .2s linear",
         }}
