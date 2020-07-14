e156a9ce2dd49a40dddf2ddceef57b9699587984
Jackson Dickison <jaksondikison@gmail.com>
2020-07-13 18:35:50 -0700

    delete test page

--- pages/layout-test.tsx
+++
@@ -1,9 +0,0 @@
-import LayoutPaddingContainer from "../components/LayoutPaddingContainer"
-
-export default () => {
-  return (
-    <LayoutPaddingContainer>
-      <div>hello from layout-test.tsx!</div>
-    </LayoutPaddingContainer>
-  )
-}
