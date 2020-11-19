48915e65e3ee0fdb9cd47e1016ff0c0c84c6d4ae
Jackson Dickison <jaksondikison@gmail.com>
2020-11-19 09:53:23 -0800

    actually add ModalProvider

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,32 @@
+import {
+  createContext,
+  useContext,
+  ReactNode,
+  useEffect,
+  useState,
+  useCallback,
+} from "react"
+
+interface IModalContext {
+  hideModal: boolean
+  setHideModal: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const ModalContext = createContext({} as IModalContext)
+export const useModalContext = () => {
+  return useContext(ModalContext)
+}
+
+export default (props: any) => {
+  const [hideModal, setHideModal] = useState(true)
+  return (
+    <ModalContext.Provider
+      value={{
+        hideModal,
+        setHideModal,
+      }}
+    >
+      {props.children}
+    </ModalContext.Provider>
+  )
+}
